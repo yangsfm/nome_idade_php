357839741d8dca6304486f301638660126b45a35
Yang Sarmento Ferreira <60950025+yangsfm@users.noreply.github.com>
2021-08-31 19:03:09 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<?php 
+	$nome = "Yang";
+	$idade = 16;
+
+	echo "$nome você têm $idade anos!";
+	echo "<br><br>";
+
+	echo "\"$nome você têm $idade anos!\"";
+	echo "<br><br>";
+
+	echo "\"$nome você têm '$idade' anos!\"";
+	echo "<br><br>";
+
+	echo "feito por $nome!";
+?>
